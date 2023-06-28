0e85e13ea1c05423ae45a4cca130e60e6e1c00b6
Ananth Narayan <narayanananth26@gmail.com>
2023-06-28 13:44:42 +0530

    remove boilerplate code

--- src/App.js
+++ src/App.js
@@ -33,18 +33,14 @@ function formatDay(dateStr) {
 }
 
 class App extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			location: "delhi",
-			isLoading: false,
-			displayLocation: "",
-			weather: {},
-		};
-		this.fetchWeather = this.fetchWeather.bind(this);
-	}
+	state = {
+		location: "delhi",
+		isLoading: false,
+		displayLocation: "",
+		weather: {},
+	};
 
-	async fetchWeather() {
+	fetchWeather = async () => {
 		try {
 			this.setState({ isLoading: true });
 			// 1) Getting location (geocoding)
@@ -73,7 +69,7 @@ class App extends React.Component {
 		} finally {
 			this.setState({ isLoading: false });
 		}
-	}
+	};
 
 	render() {
 		return (
